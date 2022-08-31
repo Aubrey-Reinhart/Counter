c1a36ab3a4738024612ce4de47fa69c328b3931a
Aubrey Reinhart <aubrey.reinhart@gmail.com>
2022-08-31 09:33:55 -0700

    Created count project

---
+++ app.js
@@ -0,0 +1,37 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    //used to see the buttons were accessing
+    console.log(btn);
+    //this checks for which button the user clicked on
+    //the console log checks what the button being clicked is
+    btn.addEventListener('click', function(e){
+        console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+        //logic for decrease, increase and reset buttons
+        if (styles.contains("decrease")){
+            count--;
+        } else if (styles.contains("increase")){
+            count++;
+        } else if (styles.contains("reset")){
+            count = 0;
+        }
+        //changing color based on count value
+        if (count > 0){
+            value.style.color = "green";
+        }
+        if (count < 0){
+            value.style.color = "red";
+        }
+        if (count == 0){
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+});
+    
